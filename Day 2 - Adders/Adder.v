bddc456057ddc5fa8d81cb676d2bcf1a959e205c
Satyanarayana Mannem <108258384+Satyamannem1017@users.noreply.github.com>
2024-07-07 20:41:58 +0530

    Update Adder.v

--- Day 2 - Adders/Adder.v
+++ Day 2 - Adders/Adder.v
@@ -1,1 +1,16 @@
+module halfadder (input a,b,output sum,carry);
+  assign sum=a^b;
+  assign carry=a&b;
+endmodule
+
+module fulladder(input a,b,cin,output sum,carry);
+  wire c1,s,c2;
+  assign sum=a^b^cin;
+  assign carry=a&b|b&cin|cin&a;
+/*
+  halfadder a1(a,b,s,c1);
+  halfadder a2(cin,s,sum,c2);
+  or o1(carry,c1,c2);
+  */
+endmodule
 
