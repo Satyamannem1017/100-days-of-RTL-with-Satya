02ab0318c85e3edec6202e8e13b8a80842a203cd
Satyanarayana Mannem <108258384+Satyamannem1017@users.noreply.github.com>
2024-07-10 20:12:10 +0530

    Create Adder_tb.v

---
+++ Day 2 - Adders/Adder_tb.v
@@ -0,0 +1,42 @@
+//testbench for half adder
+
+module adder_tb;
+  reg a,b;
+  wire sum,carry;
+
+  adder dut(a,b,sum,carry);
+
+  initial begin
+    repeat(10) begin
+      a=$urandom;
+      b=$urandom;
+      #10;
+    end
+  end
+
+  initial
+    $monitor("a=%b,b=%b,sum=%b,carry=%b", a,b,sum,carry);
+endmodule
+
+//testbench for full adder
+
+/*
+module adder_tb;
+  reg a,b,cin;
+  wire sum,carry;
+
+  adder dut(a,b,cin,sum,carry);
+
+  initial begin
+    repeat(10) begin
+      a=$urandom;
+      b=$urandom;
+      cin=$urandom;
+      #10;
+    end
+  end
+
+  initial
+    $monitor("a=%b,b=%b,cin=%b,sum=%b,carry=%b", a,b,sum,carry);
+endmodule
+*/
